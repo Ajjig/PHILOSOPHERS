7d980ad05dbdf4cde85131dec93c9cc381d0a6b9
Ajjig <ajjigsimo@gmail.com>
2022-02-10 20:53:55 +0100

    added is_all_eat() to check number_of_times_each_philo_must_eat

--- philo.c
+++ philo.c
@@ -39,6 +39,7 @@ void	new_philo(t_philo *head, int nth, int ac, char **av)
 	new -> time_to_die = atoi(av[2]);
 	new -> time_to_eat = atoi(av[3]);
 	new -> time_to_sleep = atoi(av[4]);
+	new -> number_of_times_each_philosopher_must_eat = -1;
 	if (ac == 6)
 		new -> number_of_times_each_philosopher_must_eat = atoi(av[5]);
 	new -> next = NULL;
@@ -58,6 +59,7 @@ t_philo	*creat_philos(int ac, char **av)
 	head -> time_to_die = atoi(av[2]);
 	head -> time_to_eat = atoi(av[3]);
 	head -> time_to_sleep = atoi(av[4]);
+	head -> number_of_times_each_philosopher_must_eat = -1;
 	if (ac == 6)
 		head -> number_of_times_each_philosopher_must_eat = atoi(av[5]);
 	head->next = NULL;
@@ -90,7 +92,8 @@ void	*philo_routine(void *void_arg)
 			printf("%llu %d is eating\n", runtime_to_ms(), philo -> nth);
 			usleep(philo->time_to_eat * 1000);
 			philo -> last_eat = runtime_to_ms();
-			philo -> number_of_times_each_philosopher_must_eat--;
+			if (philo -> number_of_times_each_philosopher_must_eat > 0)
+				philo -> number_of_times_each_philosopher_must_eat--;
 			pthread_mutex_unlock(&philo -> fork);
 			pthread_mutex_unlock(&philo -> next -> fork);
 		}
@@ -106,29 +109,33 @@ void	*philo_routine(void *void_arg)
 	return (NULL);
 }
 
-int	health_center(t_philo *philo)
+int	is_all_eat(t_philo *head)
 {
-	t_philo	*prev;
+	t_philo	*round;
 
+	round = head -> next;
+	while (round != head)
+	{
+		if (round -> number_of_times_each_philosopher_must_eat > 0)
+			return (0);
+		round = round -> next;
+	}
+	return (1);
+}
+
+void	*health_center(void *void_arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *) void_arg;
 	while (1)
 	{
-		prev = philo;
-		while (philo->number_of_times_each_philosopher_must_eat < 0)
-		{
-			philo = philo -> next;
-			while (philo != prev)
-			{
-				if (philo -> number_of_times_each_philosopher_must_eat > 0)
-					break ;
-				philo = philo -> next;
-				if (prev == philo)
-					return (0);
-			}
-		}
-		if (runtime_to_ms() > philo -> last_eat + philo->time_to_die)
+		if (philo -> number_of_times_each_philosopher_must_eat == 0 && is_all_eat(philo))
+			return (NULL);
+		if (runtime_to_ms() > philo -> last_eat + philo -> time_to_die)
 		{
 			printf("%d is died\n", philo -> nth);
-			return (0);
+			return (NULL);
 		}
 		philo = philo -> next;
 	}
@@ -136,9 +143,10 @@ int	health_center(t_philo *philo)
 
 int	main(int ac,char **av)
 {
-	t_philo	*head;
-	int		number_of_philosophers;
-	int		i;
+	t_philo		*head;
+	int			number_of_philosophers;
+	int			i;
+	pthread_t	health;
 
 	i = 0;
 	head = creat_philos(ac, av);
@@ -149,13 +157,12 @@ int	main(int ac,char **av)
 		pthread_create(&head -> thread_data, NULL, &philo_routine, head);
 		head = head->next;
 	}
-	if (health_center(head) == 0)
+	pthread_create(&health, NULL, &health_center, head);
+	pthread_join(health, NULL);
+	while (i--)
 	{
-		while (i--)
-		{
-			pthread_detach(head -> thread_data);
-			head = head -> next;
-		}
+		pthread_detach(head -> thread_data);
+		head = head -> next;
 	}
 	free_clear(head);
 }
