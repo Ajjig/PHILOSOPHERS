811bbec878d23ccf06dc3d8e062dda9f44d2f56d
Ajjig <ajjigsimo@gmail.com>
2022-02-11 00:17:15 +0100

    Move util functions to philo_utils.c

--- philo/philo.c
+++ philo/philo.c
@@ -1,33 +1,5 @@
 #include "philo.h"
 
-unsigned long long	runtime_to_ms()
-{
-	unsigned long long int			ms;
-	static unsigned long long int	start = 0;
-	static struct timeval			time;
-
-	gettimeofday(&time, NULL);
-	ms = time.tv_sec * 1000;
-	ms += time.tv_usec / 1000;
-	if (!start)
-		start = ms;
-	return (ms - start);
-}
-
-void	free_clear(t_philo *head)
-{
-	t_philo	*next;
-
-	next = head;
-	free(head);
-	head = head->next;
-	while (next != head)
-	{
-		free(head);
-		head = head->next;
-	}
-}
-
 void	new_philo(t_philo *head, int nth, int ac, char **av)
 {
 	t_philo	*new;
@@ -107,38 +79,6 @@ void	*philo_routine(void *void_arg)
 		}
 	}
 	return (NULL);
-}
-
-int	is_all_eat(t_philo *head)
-{
-	t_philo	*round;
-
-	round = head -> next;
-	while (round != head)
-	{
-		if (round -> number_of_times_each_philosopher_must_eat > 0)
-			return (0);
-		round = round -> next;
-	}
-	return (1);
-}
-
-void	*health_center(void *void_arg)
-{
-	t_philo	*philo;
-
-	philo = (t_philo *) void_arg;
-	while (1)
-	{
-		if (philo -> number_of_times_each_philosopher_must_eat == 0 && is_all_eat(philo))
-			return (NULL);
-		if (runtime_to_ms() > philo -> last_eat + philo -> time_to_die)
-		{
-			printf("%d is died\n", philo -> nth);
-			return (NULL);
-		}
-		philo = philo -> next;
-	}
 }
 
 int	main(int ac,char **av)
---
+++ philo/philo_utils.c
@@ -0,0 +1,61 @@
+#include "philo.h"
+
+unsigned long long int	runtime_to_ms(void)
+{
+	unsigned long long int			ms;
+	static unsigned long long int	start = 0;
+	static struct timeval			time;
+
+	gettimeofday(&time, NULL);
+	ms = time.tv_sec * 1000;
+	ms += time.tv_usec / 1000;
+	if (!start)
+		start = ms;
+	return (ms - start);
+}
+
+void	free_clear(t_philo *head)
+{
+	t_philo	*next;
+
+	next = head;
+	free(head);
+	head = head->next;
+	while (next != head)
+	{
+		free(head);
+		head = head->next;
+	}
+}
+
+int	is_all_eat(t_philo *head)
+{
+	t_philo	*round;
+
+	round = head -> next;
+	while (round != head)
+	{
+		if (round -> number_of_times_each_philosopher_must_eat > 0)
+			return (0);
+		round = round -> next;
+	}
+	return (1);
+}
+
+void	*health_center(void *void_arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *) void_arg;
+	while (1)
+	{
+		if (philo -> number_of_times_each_philosopher_must_eat == 0 && is_all_eat(philo))
+			return (NULL);
+		if (runtime_to_ms() > philo -> last_eat + philo -> time_to_die)
+		{
+			printf("%d is died\n", philo -> nth);
+			return (NULL);
+		}
+		philo = philo -> next;
+	}
+}
