267fe6fc42244dc5b0f891448343619d97e13684
Ajjig <ajjigsimo@gmail.com>
2022-02-22 00:07:52 +0100

    Norminated

--- philo_bonus/wait_for_all.c
+++ philo_bonus/wait_for_all.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wait_for_all.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 00:06:42 by majjig            #+#    #+#             */
+/*   Updated: 2022/02/22 00:07:36 by majjig           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo_bonus.h"
 
-void    *wait_for_all(void *void_arg)
+void	*wait_for_all(void *void_arg)
 {
-    t_philo *philo;
+	t_philo	*philo;
 
-    philo = (t_philo *) void_arg;
-    while (philo -> pids)
-    {
-        sem_wait(philo -> sems -> all);
-        philo -> pids = philo -> pids -> next;
-    }
-    sem_wait(philo -> sems -> all);
-    sem_post(philo -> sems -> one);
-    return (NULL);
+	philo = (t_philo *) void_arg;
+	while (philo -> pids)
+	{
+		sem_wait(philo -> sems -> all);
+		philo -> pids = philo -> pids -> next;
+	}
+	sem_wait(philo -> sems -> all);
+	sem_post(philo -> sems -> one);
+	return (NULL);
 }
